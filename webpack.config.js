e2da1bdd0b1b422dc94fabc3e2098d11cb2a18db
odgr-show <121861788+odgr-show@users.noreply.github.com>
2023-12-12 21:13:11 +0900

    html-loaderを追加しました。

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ module.exports = {
     devServer: {
         static: path.resolve(__dirname, "src"),
     },
+    mode: "production", //webpackで実行すればproduction(本番環境)で実行できる
+    devtool: "source-map", //javascriptを読みやすいコード（記述したコード）に変換する
     entry: "./src/js/main.js",
     output: {
         path: path.resolve(__dirname, "./dist"),
@@ -16,6 +18,18 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js/,
+                exclude: /node_modules/, //node_modulesの中は対象外にする
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"],
+                        },
+                    },
+                ],
+            },
+            {
                 test: /\.(css|sass|scss)/,
                 use: [
                     {
@@ -23,6 +37,9 @@ module.exports = {
                     },
                     {
                         loader: "css-loader",
+                        options: {
+                            sourceMap: false, //sassの記述を表示させる。（本番環境では処理が重くなるのでfalseにしておく）
+                        },
                     },
                     {
                         loader: "sass-loader",
@@ -31,12 +48,21 @@ module.exports = {
             },
 
             {
-                test: /\.(jpeg|jpg|png)/,
+                test: /\.(jpeg|png|jpg)/,
                 type: "asset/resource",
                 generator: {
                     filename: "images/[name][ext]",
                 },
                 use: [],
+            },
+
+            {
+                test: /\.html/,
+                use: [
+                    {
+                        loader: "html-loader",
+                    },
+                ],
             },
         ],
     },
